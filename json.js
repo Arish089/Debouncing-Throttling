3dbe2e5a38a7f862ef987824e40ae75c3e173ff5
ARISH ALI <arish.ali089@gmail.com>
2024-01-18 22:11:38 +0530

    done

--- json.js
+++
@@ -1,9 +0,0 @@
-function fetchdata(){
-    try {
-        let res = await fetch(`C:\Users\arish\AppData\Local\Microsoft\Windows\INetCache\IE\689HFCRK\ASGARALI_2324[1].json`);
-    }
-        
-     catch (error) {
-        console.log(error);
-    }
-}
